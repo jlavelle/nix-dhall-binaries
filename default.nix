813d69e31f746da00025c463878afa17c3246450
John C. Lavelle <jlavelle604@gmail.com>
2020-10-31 15:10:34 -0400

    Copy all unpacked files to bin

--- default.nix
+++ default.nix
@@ -1,18 +1,14 @@
-{ pkgs ? import <nixpkgs> {}, release ? "1.36.0", platform ? "x86_64-linux", binary ? "dhall" }:
+{ pkgs ? import <nixpkgs> {}, release ? "1.36.0", platform ? "x86_64-linux", name ? "dhall" }:
 
 let
   releases = builtins.fromJSON (builtins.readFile ./sources.json);
-  asset = releases."${release}"."${platform}"."${binary}";
-  src = pkgs.fetchurl {
-    url = asset.url;
-    sha256 = asset.sha256;
-  };
+  asset = releases."${release}"."${platform}"."${name}";
+  src = pkgs.fetchurl asset;
 in pkgs.stdenv.mkDerivation {
-  name = binary;
+  inherit name src;
   phases = [ "unpackPhase" "installPhase" ];
   installPhase = ''
     mkdir -p $out/bin
-    cp ${binary} $out/bin
+    cp * $out/bin
   '';
-  inherit src;
 }
